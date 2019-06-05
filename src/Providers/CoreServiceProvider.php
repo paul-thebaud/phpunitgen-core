f4a6f6f49cbafba6d4fab7f36aa6ac0d2ebce717
Paul Thébaud <paul.thebaud29@gmail.com>
2019-06-05 20:19:33 +0200

    Add PhpUnitGen core service provider for league container

---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpUnitGen\Core\Providers;
+
+use League\Container\Container;
+use League\Container\ReflectionContainer;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use PhpUnitGen\Core\Contracts\CodeParser as CodeParserContract;
+use PhpUnitGen\Core\Parsers\CodeParser;
+use Roave\BetterReflection\BetterReflection;
+
+/**
+ * Class CoreServiceProvider.
+ *
+ * @package PhpUnitGen\Core
+ * @author  Paul Thébaud <paul.thebaud29@gmail.com>
+ * @author  Killian Hascoët <killian.h@live.fr>
+ * @license MIT
+ */
+class CoreServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var string[] $provides
+     */
+    protected $provides = [
+        CodeParserContract::class,
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function register()
+    {
+        /** @var Container $container */
+        $container = $this->getContainer();
+
+        $container->defaultToShared();
+        $container->delegate(
+            (new ReflectionContainer())->cacheResolutions()
+        );
+
+        // Contracts implementations.
+        $container->share(CodeParserContract::class, CodeParser::class)
+            ->addArgument(BetterReflection::class);
+    }
+}
