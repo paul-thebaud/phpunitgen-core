2b21821292185ef0e67383637ac410c0135891e2
Paul Thébaud <paul.thebaud29@gmail.com>
2019-06-05 20:19:12 +0200

    Add invalid argument exception

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpUnitGen\Core\Exceptions;
+
+use InvalidArgumentException as BaseInvalidArgumentException;
+
+/**
+ * Class InvalidArgumentException.
+ *
+ * @package PhpUnitGen\Core
+ * @author  Paul Thébaud <paul.thebaud29@gmail.com>
+ * @author  Killian Hascoët <killian.h@live.fr>
+ * @license MIT
+ */
+class InvalidArgumentException extends BaseInvalidArgumentException
+{
+}
