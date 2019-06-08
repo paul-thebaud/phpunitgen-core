355e53456ab29e6e84edc0065503398a3b1f308e
Paul Thébaud <paul.thebaud29@gmail.com>
2019-06-08 14:42:30 +0200

    Add helper to test case to call protected / private method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,8 @@ namespace Tests\PhpUnitGen\Core;
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use ReflectionClass;
+use ReflectionException;
 
 /**
  * Class TestCase.
@@ -13,4 +15,23 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 class TestCase extends PHPUnitTestCase
 {
     use MockeryPHPUnitIntegration;
+
+    /**
+     * Call the given method on the given instance / class using reflection.
+     *
+     * @param object|string $instance
+     * @param string        $method
+     * @param mixed         ...$args
+     *
+     * @return mixed
+     *
+     * @throws ReflectionException
+     */
+    protected function callProtectedMethod($instance, string $method, ...$args)
+    {
+        $reflectionMethod = (new ReflectionClass($instance))->getMethod($method);
+        $reflectionMethod->setAccessible(true);
+
+        return $reflectionMethod->invoke(is_object($instance) ? $instance : null, ...$args);
+    }
 }
